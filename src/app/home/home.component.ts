36bf414967ef62b9b1ee179b90449c97ecc35932
lee <leeeorzol@gmail.com>
2018-02-16 15:47:32 -0600

    added social login

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -55,4 +55,19 @@ export class HomeComponent implements OnInit {
     this.itemCount = this.goals.length;
     this._data.changeGoal(this.goals);
   }
+  
+  onSignIn(googleUser) {
+      var profile = googleUser.getBasicProfile();
+      console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+      console.log('Name: ' + profile.getName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+
+  signOut(){
+     var auth2 = gapi.auth2.getAuthInstance();
+      auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+
 }
