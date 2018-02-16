a23e0b2b322607b389228fc3ab58f6c3d1a5b9a1
lee <leeeorzol@gmail.com>
2018-02-16 16:47:52 -0600

    starting gapi introduction

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -34,7 +34,6 @@ export class HomeComponent implements OnInit {
   btnText: string = 'Add an item'
   goalText: string = "My first life goal"
   goals = [];
-
   constructor(private _data: DataService) { }
 
   ngOnInit() {
@@ -54,5 +53,20 @@ export class HomeComponent implements OnInit {
     this.goals.splice(i,1);
     this.itemCount = this.goals.length;
     this._data.changeGoal(this.goals);
+  }
+
+  signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+
+  onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   }
 }
